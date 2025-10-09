acb4987afc4886fdb0143de24d4cac56128eeec2
ronaldquispeO <roquispec@practicante.luzdelsur.com.pe>
2025-10-09 18:04:30 -0500

    9-10-2025

---
+++ ScriptsDBeaver/Script-1.sql
@@ -0,0 +1,5 @@
+SELECT cod_scada, celda, fecha_hora, frecuencia, pot_aparente, tens_prom_s, tens_fase_neutro, tens_fase_prom, tens_ref, tens_fase_r_neutro, tens_fase_sn, tens_fase_tn, tens_fase_tr
+FROM scada_lectura
+WHERE (celda LIKE "TRA%") OR (celda LIKE "TRF%")
+ORDER BY fecha_hora DESC
+
