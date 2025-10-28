d1b5c3a3f30d886a5c80c6f7630d4c7594a3644d
ronaldquispeO <roquispec@practicante.luzdelsur.com.pe>
2025-10-28 08:45:28 -0500

    28/10

--- ScriptsDBeaver/Script-1.sql
+++ ScriptsDBeaver/Script-1.sql
@@ -5,8 +5,23 @@ SELECT
 FROM 
     scada_lectura
 WHERE 
-   	(cod_scada LIKE 'ASIA%220%TRA1') AND 
-   	(fecha_hora LIKE '2025-06-01%')
-ORDER BY 
-    fecha_hora ASC;
-	
+    celda IN ('TRA1','TRA2','TRA3','TRA4','TRA5','TRF1','TRF2','TRF3','TRF4','TRF5')
+    AND (
+        cod_scada LIKE 'ALTOPRAD/60/TRF1' OR
+        cod_scada LIKE 'ASIA/220/TRA1' OR
+        cod_scada LIKE 'BALNEARI/220/TRA2' OR
+        cod_scada LIKE 'BALNEARI/220/TRA3' OR
+        cod_scada LIKE 'BALNEARI/220/TRA4' OR
+        cod_scada LIKE 'CANTERA/220/TRA1' OR
+        cod_scada LIKE 'MANCHAY/220/TRA1' OR
+        cod_scada LIKE 'PACHACUT/220/TRA2' OR
+        cod_scada LIKE 'PROGRESO/220/TRA1' OR
+        cod_scada LIKE 'SBARTOLO/60/TRF2' OR
+        cod_scada LIKE 'SANJUAN/220/TRA1' OR
+        cod_scada LIKE 'SANJUAN/220/TRA2' OR
+        cod_scada LIKE 'SROSAN/220/TRA4%' OR
+        cod_scada LIKE 'SROSAN/220/TRA3%'OR
+        cod_scada LIKE 'SANLUIS/220/TRA2'
+    )
+ORDER BY
+    fecha_hora DESC;
